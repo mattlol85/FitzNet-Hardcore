b48cbecbb3a467d811e5f2b4b496f55bffc05cc7
Matthew Fitzgerald <mattfitzgerald85@gmail.com>
2020-01-19 23:57:51 -0500

    Initial commit

---
+++ src/main/java/org/fitznet/doomdns/fitznethardcore/FitzNetHardcore.java
@@ -0,0 +1,28 @@
+//Matthew Fitzgerald Jan 19 2020
+package org.fitznet.doomdns.fitznethardcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FitzNetHardcore extends JavaPlugin {
+
+    /**
+     ON first world join, 1 life only.
+
+     Every 8 in game hours, one life added.
+
+     LifeCap total 10?
+
+     -Future add afk check.
+     */
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
