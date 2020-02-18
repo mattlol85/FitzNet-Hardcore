eabda7d47e97053de4197308537c6f649bb51aba
BuildTools <unconfigured@null.spigotmc.org>
2020-02-18 19:46:16 +0000

    Removed unused imports and fixed TODOs

--- src/main/java/org/fitznet/doomdns/fitznethardcore/EventManager.java
+++ src/main/java/org/fitznet/doomdns/fitznethardcore/EventManager.java
@@ -1,14 +1,9 @@
 package org.fitznet.doomdns.fitznethardcore;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class EventManager {
--- src/main/java/org/fitznet/doomdns/fitznethardcore/LivesScoreboard.java
+++ src/main/java/org/fitznet/doomdns/fitznethardcore/LivesScoreboard.java
@@ -1,7 +1,5 @@
 package org.fitznet.doomdns.fitznethardcore;
 
-import org.bukkit.Bukkit;
-
 public class LivesScoreboard {
 
 }
