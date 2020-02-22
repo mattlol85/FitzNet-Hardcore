c0c5a3a1bf2d0d58748aa6458d86c3fc8f9d9eac
BuildTools <mattlol85@gmail.com>
2020-02-21 21:44:06 -0500

    Added psudo makefile

---
+++ cleanAndRun.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#Matthew Fitzgerald 2020
+
+## CHANGE THESE
+## Directory to Development Server
+SERVER_PATH='/home/matt/Desktop/DevServer'
+## Plugin Source File
+PLUGIN_PATH='/home/matt/Desktop/FitzNet-Hardcore'
+# Remove old files (Clean)
+cd $SERVER_PATH
+
+rm banned*
+
+cd plugins
+
+rm -r Fitz*
+
+# Maven Compile
+cd $PLUGIN_PATH
+
+mvn
+
+# Add new jar into file
+cp $PLUGIN_PATH/target/Fitz-NetHardcore-1.15.2-SNAPSHOT.jar /home/matt/Desktop/DevServer/plugins/ 
+
+cd $SERVER_PATH
+
+# Run Server
+java -jar spigot.jar
